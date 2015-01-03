72e8688922eee87744066564eb133a9b434f3aad
Keith Jackson <i@keithajackson.com>
2015-01-02 22:51:03 -0800

    Enabled autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,8 @@ var server_root = "out/";
 gulp.task('styles', function() {
   return gulp.src('styles/main.scss')
     .pipe(sass({ style: 'expanded', }))
-    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-//    .pipe(gulp.dest(server_root + 'css'))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+ //   .pipe(gulp.dest(server_root + 'css'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
 //    .pipe(minifycss({keepBreaks:true}))
